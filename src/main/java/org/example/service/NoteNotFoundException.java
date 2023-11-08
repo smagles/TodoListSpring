c994bab512cd040e38531fb0b8d0f9793f420349
smagles <lesialist@icloud.com>
2023-11-08 14:51:49 +0100

    Added service classes

---
+++ src/main/java/org/example/service/NoteNotFoundException.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+public class NoteNotFoundException extends RuntimeException {
+    public static final String ERROR_MESSAGE = "Note not found with id:";
+
+    public NoteNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/service/NoteService.java
@@ -0,0 +1,64 @@
+package org.example.service;
+
+import org.example.entities.Note;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.example.service.NoteNotFoundException.ERROR_MESSAGE;
+
+@Service
+public class NoteService {
+    private final List<Note> notes = new ArrayList<>();
+    private final AtomicInteger idCounter = new AtomicInteger(0);
+
+    public List<Note> listAll() {
+        return new ArrayList<>(notes);
+    }
+
+    public Note add(Note note) {
+        long id = idCounter.incrementAndGet();
+        note.setId(id);
+        notes.add(note);
+        return note;
+    }
+
+    public void deleteById(long id) {
+        boolean removed = notes.removeIf(note -> note.getId() == id);
+        if (!removed) {
+            throw new NoteNotFoundException(ERROR_MESSAGE + id);
+        }
+    }
+
+
+    public void update(Note note) {
+        long id = note.getId();
+        Note foundNote = null;
+
+        for (Note existingNote : notes) {
+            if (existingNote.getId() == id) {
+                foundNote = existingNote;
+                break;
+            }
+        }
+
+        if (foundNote != null) {
+            foundNote.setTitle(note.getTitle());
+            foundNote.setContent(note.getContent());
+        } else {
+            throw new NoteNotFoundException(ERROR_MESSAGE + id);
+        }
+    }
+
+
+    public Note getById(long id) {
+        for (Note note : notes) {
+            if (note.getId() == id) {
+                return note;
+            }
+        }
+        throw new NoteNotFoundException(ERROR_MESSAGE + id);
+    }
+}
