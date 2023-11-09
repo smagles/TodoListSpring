952e1dc390af017dbb01e2fe77a0d8d0bc939f7c
smagles <lesialist@icloud.com>
2023-11-09 12:58:25 +0100

    NoteNotFoundException

--- src/main/java/org/example/service/NoteNotFoundException.java
+++ src/main/java/org/example/service/NoteNotFoundException.java
@@ -1,7 +1,7 @@
 package org.example.service;
 
 public class NoteNotFoundException extends RuntimeException {
-    public static final String ERROR_MESSAGE = "Note not found with id:";
+    public static final String ERROR_MESSAGE = "Note not found with id ";
 
     public NoteNotFoundException(String message) {
         super(message);
