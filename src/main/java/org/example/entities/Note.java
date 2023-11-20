7e7137843e6722a08a4645981bc4b6950b24f7e7
smagles <lesialist@icloud.com>
2023-11-20 21:58:09 +0100

    Add annotations to Note entity class

--- src/main/java/org/example/entities/Note.java
+++ src/main/java/org/example/entities/Note.java
@@ -1,9 +1,14 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "notes")
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
