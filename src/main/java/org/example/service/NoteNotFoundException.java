6da8e876320851474ef603e173e33d3f7c5f1e9b
smagles <lesialist@icloud.com>
2023-11-20 20:00:40 +0100

    deleted NoteNotFoundException.java

--- src/main/java/org/example/service/NoteNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-public class NoteNotFoundException extends RuntimeException {
-    public static final String ERROR_MESSAGE = "Note not found with id ";
-
-    public NoteNotFoundException(String message) {
-        super(message);
-    }
-}
