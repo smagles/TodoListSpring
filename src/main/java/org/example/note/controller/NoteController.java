9d591548e137de353a2c8c5fec25e5e0547f7386
smagles <lesialist@icloud.com>
2023-12-11 17:16:04 +0100

    Add annotation @RequiredArgsConstructor

--- src/main/java/org/example/note/controller/NoteController.java
+++ src/main/java/org/example/note/controller/NoteController.java
@@ -1,8 +1,8 @@
 package org.example.note.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.example.note.model.Note;
 import org.example.note.service.NoteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,15 +10,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/note")
 public class NoteController {
 
     private final NoteService noteService;
-
-    @Autowired
-    public NoteController(NoteService noteService) {
-        this.noteService = noteService;
-    }
 
     @GetMapping("/list")
     public String getAllNotes(Model model) {
