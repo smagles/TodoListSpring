0250f6116620a00597cee1ad11dd9be481bdc0b1
smagles <lesialist@icloud.com>
2023-12-14 21:40:31 +0100

    Add unit test for NoteService.findAllNotes

---
+++ src/test/java/org/example/note/service/NoteServiceTest.java
@@ -0,0 +1,39 @@
+package org.example.note.service;
+
+import org.example.note.model.Note;
+import org.example.note.repo.NoteRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class NoteServiceTest {
+
+    @Mock
+    private NoteRepository noteRepository;
+
+    @InjectMocks
+    private NoteService noteService;
+
+    @Test
+    public void testFindAllNotes() {
+        // Given
+        Note note1 = new Note(1L, "Title 1", "Content 1");
+        Note note2 = new Note(2L, "Title 2", "Content 2");
+        List<Note> expectedNotes = Arrays.asList(note1, note2);
+
+        // When
+        Mockito.when(noteRepository.findAll()).thenReturn(expectedNotes);
+        List<Note> actualNotes = noteService.findAllNotes();
+
+        // Then
+        assertEquals(expectedNotes, actualNotes);
+    }
+
+}
