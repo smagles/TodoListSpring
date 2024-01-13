7dead372b5e46be057c32da11e8d4a33b62eabe6
smagles <lesialist@icloud.com>
2024-01-13 10:43:44 +0100

    create interface for JwtService

--- src/main/java/org/example/security/config/jwt/JwtService.java
+++ src/main/java/org/example/security/config/jwt/JwtService.java
@@ -1,81 +1,11 @@
 package org.example.security.config.jwt;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import lombok.extern.log4j.Log4j;
-import org.example.user.User;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
 
-import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
+public interface JwtService {
+    String extractUsername(String token);
 
-@Service
-@Log4j
-public class JwtService {
-    @Value("${jwt.secret-key}")
-    private String secretKey;
-    @Value("${jwt.lifetime}")
-    private int tokenLifetimeMillis;
+    String generateToken(UserDetails userDetails);
 
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
-    public String generateToken(UserDetails userDetails) {
-        log.debug("Generating token for user: " + userDetails.getUsername());
-        return generateToken(new HashMap<>(), userDetails);
-    }
-
-    public String generateToken(Map<String, Object> extraClaims,
-                                UserDetails userDetails) {
-        if (userDetails instanceof User user) {
-            extraClaims.put("role", user.getRole().name());
-        }
-        return Jwts
-                .builder()
-                .claims(extraClaims)
-                .subject(userDetails.getUsername())
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + tokenLifetimeMillis * 3600 * 24))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-
-    boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String userName = extractUsername(token);
-        return (userName.equals(userDetails.getUsername()));
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-    }
-
-    private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+    boolean isTokenValid(String token, UserDetails userDetails);
 }
