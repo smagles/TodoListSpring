8d1965fc79ad846194860ca3210fcb5dee4b774e
smagles <lesialist@icloud.com>
2024-01-13 20:29:58 +0100

    Create notes rest api responses

---
+++ src/main/java/org/example/note/response/UpdateNoteResponse.java
@@ -0,0 +1,29 @@
+package org.example.note.response;
+
+import lombok.Builder;
+import lombok.Data;
+import org.example.note.Note;
+
+@Builder
+@Data
+public class UpdateNoteResponse {
+    private Error error;
+
+    private Note updatedNote;
+
+    public enum Error {
+        ok,
+        insufficientPrivileges,
+        invalidNoteId,
+        invalidTitleLength,
+        invalidContentLength
+    }
+
+    public static UpdateNoteResponse success(Note updatedNote) {
+        return builder().error(Error.ok).updatedNote(updatedNote).build();
+    }
+
+    public static UpdateNoteResponse failed(Error error) {
+        return builder().error(error).updatedNote(null).build();
+    }
+}
