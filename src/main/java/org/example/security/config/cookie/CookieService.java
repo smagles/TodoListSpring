1bda63e3d2cb772ccea0305588ead5ad0acfa420
smagles <lesialist@icloud.com>
2024-01-13 10:44:03 +0100

    create interface for CookieService

--- src/main/java/org/example/security/config/cookie/CookieService.java
+++ src/main/java/org/example/security/config/cookie/CookieService.java
@@ -2,39 +2,12 @@ package org.example.security.config.cookie;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Service;
 
-@Service
-public class CookieService {
+public interface CookieService {
+    Cookie createCookie(String cookieName, String value);
 
-    public Cookie createCookie(String cookieName, String value){
-        Cookie token = new Cookie(cookieName,value);
-        token.setHttpOnly(true);
-        token.setMaxAge((int) 2 * 360 * 1000);
-        token.setPath("/");
-        return token;
-    }
-    public Cookie deleteCookie(String cookieName){
-        Cookie token = new Cookie(cookieName,"");
-        token.setHttpOnly(true);
-        token.setMaxAge(0);
-        token.setPath("/");
-        return token;
-    }
+    Cookie deleteCookie(String cookieName);
 
-    public Cookie getCookie(HttpServletRequest req, String cookieName){
-        final Cookie[] cookies = req.getCookies();
-        if(cookies==null) return null;
-        for(Cookie cookie : cookies){
-            if(cookie.getName().equals(cookieName))
-                return cookie;
-        }
-        return null;
-    }
-    public void resetToken(HttpServletResponse response){
-        Cookie c = deleteCookie("token");
-        response.addCookie(c);
-    }
+    Cookie getCookie(HttpServletRequest req, String cookieName);
 
 }
