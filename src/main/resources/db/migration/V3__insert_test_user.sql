094e40cf17053884d1a4528f94ba6420f74c23e8
smagles <lesialist@icloud.com>
2024-01-19 12:01:52 +0100

    created test user

---
+++ src/main/resources/db/migration/V3__insert_test_user.sql
@@ -0,0 +1,9 @@
+INSERT INTO USERS (username, email, password, role)
+VALUES ('user', 'user@example.com', '$2a$10$isV8m4XCN629F38ML8k7BujTf0CHIQGlTT5DEVf6N5f5Gpisewjz2', 'USER');
+
+INSERT INTO NOTES (title, content, id_user)
+VALUES ('Hello world', 'Hello, my name is Olesia.
+This is my first web application using Spring Framework-
+"To-Do List" With "To-Do List" you can easily ✅create, ✅edit, and ❌delete your notes✏️,
+as well as keep important records and ideas.
+Thank you❤️ for checking out my "To-Do List" project!', 1);
